c5fd64797e1dc517b9158fdf9c17be39b7c64406
nava128 <96544099+nava128@users.noreply.github.com>
2024-09-06 09:32:42 -0600

    Add files via upload
    subí dos archivos

---
+++ miscadenas.py
@@ -0,0 +1,23 @@
+'''eliseo Nava
+Trabajando con cadenas
+'''
+nombre="Eliseo"
+print(nombre[0])
+print(nombre)
+
+MisNovias=["E","l","i"]
+for chicas in MisNovias:
+  print(chicas)
+  
+  print("otro ejemplo")
+  b = "Eliseo Nava"
+print(b[2:5])
+print(" a mayusculas")
+a = "Eliseo nava"
+print(a.upper())
+print("Concatenar")
+a = "Maria"
+b = "Perez"
+c = a +"_"+ b
+print(c)
+  
---
+++ mivariables.py
@@ -0,0 +1,16 @@
+import math
+edad=65   # 65 se guarda en edad
+
+nombre="Eliseo Nava"
+print("tu edad es ",edad)
+print("Te llamas ",nombre)
+print("-- tipo de variables ---")
+x = str(7)    # x will be '3'
+y = int(3.1415)    # y will be 3
+z=float(7)
+print(x)
+print(y)
+print(z)
+print("-- funciones matematicas ----")
+x = math.sqrt(64)
+print(x)
